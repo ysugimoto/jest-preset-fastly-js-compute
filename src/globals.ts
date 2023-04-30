800e42a090a4609027f9810414db0585b773c0b7
ysugimoto <sugimoto@wnotes.net>
2023-04-30 23:49:27 +0900

    add globalThis.fastly object

--- src/globals.ts
+++ src/globals.ts
@@ -1,6 +1,14 @@
 /// <reference types="@fastly/js-compute" />
 import { getRandomValues, randomUUID, subtle } from "crypto";
 import * as nodeFetch from "node-fetch";
+import {
+  env,
+  Logger,
+  getGeolocationForIpAddress,
+  Geolocation,
+  enableDebugLogging,
+  includeBytes,
+} from "./fastly-js-compute-mock";
 
 // ref: https://github.com/fastly/js-compute-runtime/blob/main/types/globals.d.ts
 export class CompressionStream {
@@ -49,3 +57,40 @@ export function addEventListener<K extends keyof EventListenerMap>(
 ): void {
   // noop
 }
+
+// js-compute-runtime also provides globalThis.fastly object which includes some fields and method.
+// We should export them as global
+// ref: https://github.com/fastly/js-compute-runtime/issues/517
+export const fastly = {
+  __defaultBackend: "",
+
+  set baseURL(base: URL | null | undefined) {
+    // noop
+  },
+  get baseURL(): URL | null {
+    return null;
+  },
+  set defaultBackend(backend: string) {
+    this.__defaultBackend = backend;
+  },
+  get defaultBackend(): string {
+    return this.__defaultBackend;
+  },
+  env: {
+    get(name: string): string {
+      return env(name);
+    },
+  },
+  getLogger(endpoint: string): Logger {
+    return new Logger(endpoint);
+  },
+  enableDebugLogging(enabled: boolean) {
+    enableDebugLogging(enabled);
+  },
+  getGeolocationForIpAddress(address: string): Geolocation {
+    return getGeolocationForIpAddress(address);
+  },
+  includeBytes(path: string): Uint8Array {
+    return includeBytes(path);
+  },
+};
