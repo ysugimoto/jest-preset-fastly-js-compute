8dc939e8441b152530da29bcae1302c79542ea85
Jake Champion <jchampion@fastly.com>
2023-07-19 16:33:50 +0100

    feat!: update dependencies and rename ObjectStore to KVStore to match latest changes in @fastly/js-compute

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,8 +1,10 @@
 declare module "ts-jest/presets/default-esm/jest-preset" {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const content: { [key: string]: any };
   export default content;
 }
 declare module "ts-jest/presets/default/jest-preset" {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const content: { [key: string]: any };
   export default content;
 }
