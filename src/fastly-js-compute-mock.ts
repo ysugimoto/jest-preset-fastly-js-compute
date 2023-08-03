40dc1aac09c495c2b20c05c6ea4bba9dc11366ce
RileyMulford <riley.mulford@target.com>
2023-08-03 09:11:46 -0500

    chore: export secret store entry

--- src/fastly-js-compute-mock.ts
+++ src/fastly-js-compute-mock.ts
@@ -239,9 +239,7 @@ export class KVStore {
   }
 }
 
-// ref: https://github.com/fastly/js-compute-runtime/blob/main/types/fastly:secret-store.d.ts
-// Note that SecretStore related class does not seem to be public yet, but just declare for future implementation.
-class SecretStoreEntry {
+export class SecretStoreEntry {
   private name: string;
 
   constructor(name: string) {
