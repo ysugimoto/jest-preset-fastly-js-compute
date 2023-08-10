c925b458b40ede76338e852e60f9b98cf7fffd71
Jake Champion <me@jakechampion.name>
2023-08-10 10:32:37 +0100

    re-add accidentally removed  defaultESM options

--- src/typescript/esm/jest-preset.ts
+++ src/typescript/esm/jest-preset.ts
@@ -1,5 +1,6 @@
 /// <reference types="@fastly/js-compute" />
 
+import defaultESM from "ts-jest/presets/default-esm/jest-preset";
 import {
   addEventListener,
   CompressionStream,
@@ -10,6 +11,7 @@ import {
 } from "../../globals";
 import { join } from "node:path";
 export default {
+  ...defaultESM,
   globals: {
     addEventListener,
     CompressionStream,
