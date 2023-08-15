2a335cfe944d7aaf88d4bbc4f2a1a150e548fbfb
ysugimoto <sugimoto@wnotes.net>
2023-08-15 17:55:59 +0900

    chore: fix typo

--- src/typescript/esm/jest-preset.ts
+++ src/typescript/esm/jest-preset.ts
@@ -6,7 +6,7 @@ import { join } from "node:path";
 export default {
   ...defaultESM,
   // Jest document says that "globals" object must be json-serializable,
-  // so that we could not specify class instance which has methods and getter/setter due to lack on json serializinga.
+  // so that we could not specify class instance which has methods and getter/setter due to lack on json serializing.
   // Then we should use setupFiles instead in order to specify global functions and classes
   //
   // @see: https://github.com/jestjs/jest/blob/main/docs/Configuration.md#globals-object
